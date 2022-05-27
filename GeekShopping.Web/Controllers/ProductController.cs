7b850fcd60e08e3ca2cddab65b00ccfc3af732a9
samuel.martins <samuel.martins@starline.inf.br>
2022-05-27 18:35:43 -0300

    Add metodos e views create e update

--- GeekShopping.Web/Controllers/ProductController.cs
+++ GeekShopping.Web/Controllers/ProductController.cs
@@ -1,3 +1,4 @@
+using GeekShopping.Web.Models;
 using GeekShopping.Web.Services.IServices;
 using Microsoft.AspNetCore.Mvc;
 
@@ -16,5 +17,53 @@ public class ProductController : Controller
    {
       var products = await _productService.FindAllProducts();
       return View(products);
+   }
+
+   public IActionResult Create()
+   {
+      return View();
+   }
+
+   [HttpPost]
+   public async Task<IActionResult> Create(ProductModel model)
+   {
+      if (ModelState.IsValid)
+      {
+         var response = await _productService.CreateProduct(model);
+
+         if (response != null)
+         {
+            return RedirectToAction(nameof(Products));
+         }
+      }
+
+      return View(model);
+   }
+
+   public async Task<IActionResult> Update(int id)
+   {
+      var product = await _productService.FindProductById(id);
+      if (product != null)
+      {
+         return View(product);
+      }
+
+      return NotFound();
+   }
+
+   [HttpPost]
+   public async Task<IActionResult> Update(ProductModel model)
+   {
+      if (ModelState.IsValid)
+      {
+         var response = await _productService.UpdateProduct(model);
+
+         if (response != null)
+         {
+            return RedirectToAction(nameof(Products));
+         }
+      }
+
+      return View(model);
    }
 }
--- GeekShopping.Web/Models/ProductModel.cs
+++ GeekShopping.Web/Models/ProductModel.cs
@@ -1,11 +1,18 @@
+using System.ComponentModel;
+
 namespace GeekShopping.Web.Models
 {
    public class ProductModel
    {
       public long Id { get; set; }
+
+      [DisplayName("Produto")]
       public string Name { get; set; }
+      [DisplayName("Preço")]
       public decimal Price { get; set; }
+      [DisplayName("Descrição")]
       public string? Description { get; set; }
+      [DisplayName("Categoria")]
       public string? Category { get; set; }
       public string? ImageURL { get; set; }
    }
