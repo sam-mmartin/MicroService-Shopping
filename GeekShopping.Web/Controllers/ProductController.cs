6acdc092a21fa077fd30f5d6aad8dd7511aac10e
samuel.martins <samuel.martins@starline.inf.br>
2022-05-27 15:16:05 -0300

    Atualizado build e start de projetos

---
+++ GeekShopping.Web/Controllers/ProductController.cs
@@ -0,0 +1,20 @@
+using GeekShopping.Web.Services.IServices;
+using Microsoft.AspNetCore.Mvc;
+
+namespace GeekShopping.Web.Controllers;
+
+public class ProductController : Controller
+{
+   private readonly IProductService _productService;
+
+   public ProductController(IProductService productService)
+   {
+      _productService = productService ?? throw new ArgumentNullException(nameof(productService));
+   }
+
+   public async Task<IActionResult> Product()
+   {
+      var products = await _productService.FindAllProducts();
+      return View(products);
+   }
+}
